dba5fe6c406253b882cff80d2baa4d82c06438dc
Kulanya Lisaldi <kulanya.lisaldi@gmail.com>
2026-03-25 01:49:30 +0530

    Remove Test API endpoint too

--- server/src/main/java/com/example/server/repository/TestItemRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.server.repository;
-
-import com.example.server.model.TestItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestItemRepo extends MongoRepository<TestItem, String> {
-}
